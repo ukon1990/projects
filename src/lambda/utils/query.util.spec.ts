6623017ff7fbb2422012fb89e3da629d3bb32b38
Ukon1990 <mail@jonaskf.net>
2019-07-24 23:12:48 +0200

    moved from using datetime to bigint(11) so that we won't have to care about timezones.
    I hope
    
    And tests

--- src/lambda/utils/query.util.spec.ts
+++ src/lambda/utils/query.util.spec.ts
@@ -1,23 +1,29 @@
 import {QueryUtil} from './query.util';
 
 class TestModel {
-  constructor(public id: number, public name: string, public isTrue: boolean, list: any[]) {
+  constructor(
+    public id: number,
+    public name: string,
+    public isTrue: boolean,
+    public list: any[],
+    public date: any) {
   }
 }
 
 describe('QueryUtil', () => {
   const util = new QueryUtil<TestModel>('test_table');
-  describe('can generate query', () => {
-    it('insert', () => {
+  describe('insert', () => {
+    it('default', () => {
       const obj = new TestModel(
         0,
         'Testing',
         false,
-        ['Stuff', 'is', 'good']
+        ['Stuff', 'is', 'good'],
+        new Date()
       );
       expect(util.insert(obj))
-        .toBe('INSERT INTO test_table(id,name,isTrue) ' +
-          'VALUES(0,"Testing",0);');
+        .toBe('INSERT INTO test_table(id,name,isTrue,date) ' +
+          'VALUES(0,"Testing",0,' + +obj.date + ');');
     });
 
     it('insert with true bool', () => {
@@ -25,25 +31,56 @@ describe('QueryUtil', () => {
         0,
         'Testing',
         true,
-        ['Stuff', 'is', 'good']
+        ['Stuff', 'is', 'good'],
+        new Date()
       );
       expect(util.insert(obj))
-        .toBe('INSERT INTO test_table(id,name,isTrue) ' +
-          'VALUES(0,"Testing",1);');
+        .toBe('INSERT INTO test_table(id,name,isTrue,date) ' +
+          'VALUES(0,"Testing",1,' + +obj.date + ');');
     });
 
+    it('insert with undefined', () => {
+      const obj = new TestModel(
+        0,
+        'Testing',
+        true,
+        ['Stuff', 'is', 'good'],
+        null
+      );
+      expect(util.insert(obj))
+        .toBe('INSERT INTO test_table(id,name,isTrue,date) ' +
+          'VALUES(0,"Testing",1,null);');
+    });
+
+    it('insert with object', () => {
+      const obj = new TestModel(
+        0,
+        'Testing',
+        true,
+        ['Stuff', 'is', 'good'],
+        {name: 'test'}
+      );
+      expect(util.insert(obj))
+        .toBe('INSERT INTO test_table(id,name,isTrue,date) ' +
+          'VALUES(0,"Testing",1,"{\"name\":\"test\"}");');
+    });
+  });
+
+  describe('update', () => {
     it('update', () => {
       const obj = new TestModel(
         0,
         'Testing',
         false,
-        ['Stuff', 'is', 'good']
+        ['Stuff', 'is', 'good'],
+        new Date()
       );
       expect(util.update(obj.id, obj))
         .toBe('UPDATE test_table ' +
           'SET id = 0,' +
           'SET name = "Testing",' +
-          'SET isTrue = 0 ' +
+          'SET isTrue = 0,' +
+          'SET date = ' + +obj.date + ' ' +
           'WHERE id = 0;');
     });
   });
